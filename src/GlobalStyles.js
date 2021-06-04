0c09c2bd7944c702bc8ede04a5b67fcf0940a775
Robert Schade <roscha80@web.de>
2021-06-04 16:28:34 +0200

    Add styles to Globalstyles.js

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,17 +5,27 @@ const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
   padding: 0;
   margin: 0;
+  background-color: var(--background);
+  color: var(--fontcolor);
+  
 }
 body {
   font-family: sans-serif;
   font-size: 112.5%;
   height: 100vh;
+  width: 100vh;
 }
 input,
 button,
 textarea {
   font-size: inherit;
 }
+
+:root {
+  --background: whitesmoke;
+  --fontcolor: steelblue;
+}
+
 `
 
 export default GlobalStyles
